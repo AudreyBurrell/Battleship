ddc160e545320dad94935e033a0d0100956622c2
AudreyBurrell <audreyb2165@gmail.com>
2026-03-14 21:14:46 -0600

    Getting the login page to be showing up and installing react router dom

---
+++ src/Login.jsx
@@ -0,0 +1,11 @@
+import './Login.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+    return (
+        <h1>Login page</h1>
+    )
+}
+
+export default Login;
